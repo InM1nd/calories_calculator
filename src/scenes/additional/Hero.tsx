41f312126165473ce2a50f2436d43efe53a806d7
InM1nd <zabolotny1111@gmail.com>
2023-12-30 22:50:35 +0200

    v0.4
    minor fixes

--- src/scenes/additional/Hero.tsx
+++ src/scenes/additional/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
   <section className=""> 
   <div className="relative">
     <div className="flex text-black h-dvh">
-      <div className="container mx-auto flex flex-col md:flex-column items-center my-12 md:my-24">
+      <div className="container mx-auto flex flex-col md:flex-column items-center mt-12 md:my-24">
 		<div className="mb-6 md:mb-0 md:mt-0 ml-0 lg:w-full lg:h-full flex justify-center">
           {/* <div className="h-48 flex flex-wrap content-center"> */}
             <img className="inline-block md:mt-0 p-0 md:p-0 lg:w-auto lg:h-auto"  src={ HeroImg }/>
@@ -17,7 +17,7 @@ const Hero = () => {
           <h1 className="text-3xl md:text-7xl text-green-700 tracking-loose">Nutrify</h1>
           <h2 className="text-3xl md:text-5xl leading-relaxed md:leading-snug mb-2">Explore your food
           </h2>
-          <p className="text-sm md:text-base mb-4">Explore your favourite food and
+          <p className="text-center text-sm md:text-base mb-4">Explore your favourite food and
             register now to build your weekly nutrition plan.</p>
           <a href="#"
             className="bg-transparent hover:bg-green-700 text-green-700 hover:text-white rounded-3xl shadow hover:shadow-lg py-4 px-8 border border-green-700 hover:border-transparent">
